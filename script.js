301b8d1970ab68007ee7b77cd3346cebba4d1ce0
Nathanfirecaps <monasterial.nathanaaron@ue.edu.ph>
2024-10-10 15:12:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signup');
+const loginButton = document.getElementById('loginButton');
+
+signUpButton.addEventListener('click', function() {
+    signInForm.style.display = "none";
+    signUpForm.style.display = "block";
+});
+
+signInButton.addEventListener('click', function() {
+    signInForm.style.display = "block";
+    signUpForm.style.display = "none";
+});
+
+loginButton.addEventListener('click', function() {
+    // Add any validation logic here if necessary
+    location.href = 'scanner.html'; // Change to your target page
+});
